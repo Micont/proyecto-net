abbe6449d4102c34f28c5d9fa04ea276a712dde4
alexanderdgtalliance <alexander.portuguez@dgtalliance.com>
2023-03-03 23:17:30 -0500

    Archivos listo

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Song data
+
+const songList = [
+    {
+        title: "Rels B - Boys Dont t Cry", 
+        file: "Rels-B_Boys_Don_t_Cry.mp3" ,
+        conver: "cover-rels.jpg"
+    },
+    
+    {
+        title: "Duki - Pintao", 
+        file: "DUKI-Pintao-Rei-YSY.mp3",
+        conver: "cover-duki.jpg"
+    },
+
+    {
+        title: "Cancerbero - Pensando en ti", 
+        file: "Pensando_en_ti.mp3",
+        conver: "cover-canserbero.jpg"
+    },
+
+]
